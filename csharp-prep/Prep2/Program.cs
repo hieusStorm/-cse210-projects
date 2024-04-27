edcc2c172b780bc0345986caff8fc76ba12d030f
hieusStorm <hyrum555@gmail.com>
2024-04-27 16:33:22 -0700

    Finished the prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int gradePercentage = int.Parse(userGrade);
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60) {
+            letter = "D";
+        }
+        else if (gradePercentage < 60) {
+            letter = "F";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed your class.");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time");
+        }
+
+        Console.WriteLine($"You have an {letter}");
     }
 }
