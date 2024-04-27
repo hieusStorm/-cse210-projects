fe1a7f6798586940e2861ea8f7701c07d7cb6710
hieusStorm <hyrum555@gmail.com>
2024-04-27 16:10:21 -0700

    Finished prep 1 project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string user_first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string user_last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {user_last_name}, {user_first_name} {user_last_name}.");
+
     }
 }
