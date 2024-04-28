77f406c5465fbd22c9d6c1d9a102589792eddbf3
hieusStorm <hyrum555@gmail.com>
2024-04-27 17:24:48 -0700

    Wrote the prep 5 project

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userStringNumber = Console.ReadLine();
+        int userIntNumber = int.Parse(userStringNumber);
+        return userIntNumber;
+    }
+
+    static int SquareNumber(int baseNumber)
+    {
+        int squaredNumber = baseNumber * baseNumber;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
