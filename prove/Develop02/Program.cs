c78212b05c2957253bf0b0445654b5374079c5e9
hieusStorm <hyrum555@gmail.com>
2024-05-04 08:08:30 -0700

    Added the ability to get the date when making a new entry

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,8 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Journal mainJournal = new Journal();
-        Entry todaysEntry = new Entry();
-
-        todaysEntry.Display();
+        List<Entry> todaysEntry = new List<Entry>();
+        Console.
     }
 }
