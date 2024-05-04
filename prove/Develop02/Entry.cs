6a6b7f7555eef67dc6a879936b60bc0ea3fe5c2f
hieusStorm <hyrum555@gmail.com>
2024-05-04 08:46:18 -0700

    Fixed the Entry display to intialize a prompt only on the first display of the entry

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,15 +1,20 @@
 public class Entry
 {
     public PromptGenerator _prompts = new PromptGenerator();
-    public string _prompt;
+    public string _prompt = "";
     public string _response = "";
     public string _date = DateTime.Now.ToString("MM/dd/yyyy");
 
     public void Display()
     {
-        _prompt = _prompts.GetRandomPrompt();
+        if (_prompt != "")
+        {
+            _prompt = _prompts.GetRandomPrompt();
+        }
+
         Console.WriteLine(_date);
         Console.WriteLine(_prompt);
+        
         if (_response != "")
         {
             Console.WriteLine(_response);
