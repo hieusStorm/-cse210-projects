3abea1e11c3e2d6b3bda38ffbc8a2b315e789ae0
hieusStorm <hyrum555@gmail.com>
2024-05-04 08:11:30 -0700

    Added the ability to get the date for the entries upon creation

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,15 +2,21 @@ public class Entry
 {
     public PromptGenerator _prompts = new PromptGenerator();
     public string _prompt;
-    public string _response;
-    public string _date;
+    public string _response = "";
+    public string _date = DateTime.Now.ToString("MM/dd/yyyy");
 
     public void Display()
     {
         _prompt = _prompts.GetRandomPrompt();
         Console.WriteLine(_date);
         Console.WriteLine(_prompt);
-        _response = Console.ReadLine();
-
+        if (_response != "")
+        {
+            Console.WriteLine(_response);
+        }
+        else
+        {
+            _response = Console.ReadLine();
+        }
     }
 }
