91dc6f333e475e3682e38b2fab15560900698cc3
hieusStorm <hyrum555@gmail.com>
2024-05-04 08:47:18 -0700

    Fixed an error

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -7,14 +7,14 @@ public class Entry
 
     public void Display()
     {
-        if (_prompt != "")
+        if (_prompt == "")
         {
             _prompt = _prompts.GetRandomPrompt();
         }
 
         Console.WriteLine(_date);
         Console.WriteLine(_prompt);
-        
+
         if (_response != "")
         {
             Console.WriteLine(_response);
