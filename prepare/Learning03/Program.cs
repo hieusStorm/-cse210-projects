c9ecd6f4ed5cef7040b20994120b5ec6acd0b183
hieusStorm <hyrum555@gmail.com>
2024-05-06 17:54:59 -0700

    Made a fraction program to practice encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Fraction test2 = new Fraction(5);
+        Fraction test3 = new Fraction(5, 3);
+
+        Console.WriteLine(test1.GetFactionString());
+
+        test2.SetDenominator(6);
+        
+        Console.WriteLine(test2.GetDenominator());
+
+        test3.SetNumerator(1);
+
+        Console.WriteLine(test3.GetNumerator());
+
+        Console.WriteLine($"{test1.GetDecimalValue()} {test2.GetFactionString()} {test2.GetDecimalValue()} {test3.GetFactionString()} {test3.GetDecimalValue()}");
     }
 }
