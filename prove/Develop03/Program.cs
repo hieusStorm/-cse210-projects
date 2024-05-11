3745b93750dd1877ff9d5be22f2a8202a8353ce7
hieusStorm <hyrum555@gmail.com>
2024-05-11 16:54:14 -0700

    Added strech challenge addition

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,14 +1,40 @@
+// to exceed requirments I made it so that the program asks the user for the scripture to memorize before working on memorizing the scripture
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string response;
+        string book;
+        int chapter;
+        int startVerse;
+        int endVerse;
+        string scriptureText;
+        string quit;
         int hideNumber = 0;
+        Reference memorizeReference;
         //Set up scripture to memorize
-        Reference psalmsReference = new Reference("psalms", 23, 4);
-        Scripture memorizeScripture = new Scripture(psalmsReference, "Yea, though I walk through the valley of the shadow of death, I will fear no evil: for thou art with me; thy rod and thy staff they comfort me.");
+        Console.WriteLine("Welcome to the scripture memorizer!");
+        Console.WriteLine("Please enter the book of the scripture you want to memorize");
+        book = Console.ReadLine();
+        Console.WriteLine("Please enter the Chapter");
+        chapter = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the start verse");
+        startVerse = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the end verse (enter the start verse again if the same as start verse)");
+        endVerse = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the text of your scripture to be memorized");
+        scriptureText = Console.ReadLine();
+
+        if (startVerse == endVerse)
+        {
+            memorizeReference = new Reference(book, chapter, startVerse);
+        }
+        else
+        {
+            memorizeReference = new Reference(book, chapter, startVerse, endVerse);
+        }
+        Scripture memorizeScripture = new Scripture(memorizeReference, scriptureText);
         int hideNumberMax = memorizeScripture.getLength();
         //set up process to memorize the scripture
         do
@@ -16,11 +42,11 @@ class Program
             Console.Clear();
             Console.WriteLine(memorizeScripture.GetDisplayText());
             Console.WriteLine("Please press enter to continue or type 'quit' to finish");
-            response = Console.ReadLine();
+            quit = Console.ReadLine();
             //Hide some words
             hideNumber++;
             memorizeScripture.HideWords(hideNumber);
-        } while (response != "quit" && memorizeScripture.getHiddenLength() < hideNumberMax);
+        } while (quit != "quit" && memorizeScripture.getHiddenLength() < hideNumberMax);
 
     }
 }
