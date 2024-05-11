d528ee33a29b413664f3b722ebcadd0e5a3b699a
hieusStorm <hyrum555@gmail.com>
2024-05-11 15:31:05 -0700

    fixed how I'm hiding words to correct the number of words each time

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string response;
-        int hideNumber = 1;
+        int hideNumber = 0;
         //Set up scripture to memorize
         Reference psalmsReference = new Reference("psalms", 23, 4);
         Scripture memorizeScripture = new Scripture(psalmsReference, "Yea, though I walk through the valley of the shadow of death, I will fear no evil: for thou art with me; thy rod and thy staff they comfort me.");
@@ -18,8 +18,8 @@ class Program
             Console.WriteLine("Please press enter to continue or type 'quit' to finish");
             response = Console.ReadLine();
             //Hide some words
-            memorizeScripture.HideWords(hideNumber);
             hideNumber++;
+            memorizeScripture.HideWords(hideNumber);
         } while (response != "quit" || hideNumber > memorizeScripture.getLength());
 
     }
