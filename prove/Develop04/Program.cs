d93eefc5444eb4d9c07f0514ea8ea7cb565a8b3b
hieusStorm <hyrum555@gmail.com>
2024-05-16 11:02:14 -0700

    made the user interface

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity testBreathing = new BreathingActivity();
-        testBreathing.Run();
+        int userChoice;
+
+        do
+        {
+            Console.WriteLine("Options");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the options: ");
+            userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+            }
+            else if (userChoice == 2)
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.Run();
+            }
+            else if (userChoice == 3)
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();   
+            }
+        } while (userChoice != 4);
     }
 }
