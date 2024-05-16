bb822e92d872118589f0cfdef83f2c6d00b7b54b
hieusStorm <hyrum555@gmail.com>
2024-05-16 11:21:02 -0700

    Add the ability to keep track of how many times each activity as been done

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,3 +1,4 @@
+//Created a log that displays all the activities that have been done
 using System;
 
 class Program
@@ -5,6 +6,7 @@ class Program
     static void Main(string[] args)
     {
         int userChoice;
+        int[] activities = [0,0,0];
 
         do
         {
@@ -12,7 +14,8 @@ class Program
             Console.WriteLine("1. Start Breathing Activity");
             Console.WriteLine("2. Start Reflecting Activity");
             Console.WriteLine("3. Start Listing Activity");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("4. Display what activities have been done");
+            Console.WriteLine("5. Quit");
             Console.Write("Select a choice from the options: ");
             userChoice = int.Parse(Console.ReadLine());
 
@@ -20,17 +23,30 @@ class Program
             {
                 BreathingActivity breathingActivity = new BreathingActivity();
                 breathingActivity.Run();
+                activities[0]++;
             }
             else if (userChoice == 2)
             {
                 ReflectingActivity reflectingActivity = new ReflectingActivity();
                 reflectingActivity.Run();
+                activities[1]++;
             }
             else if (userChoice == 3)
             {
                 ListingActivity listingActivity = new ListingActivity();
-                listingActivity.Run();   
+                listingActivity.Run();
+                activities[2]++;   
             }
-        } while (userChoice != 4);
+            else if (userChoice == 4)
+            {
+                Console.Clear();
+                Console.WriteLine($"You have compleated the Breathing Activity {activities[0]} time/s.");
+                Console.WriteLine($"You have completed the Reflecting Activity {activities[1]} time/s.");
+                Console.WriteLine($"You have completed the Listing Activity {activities[2]} time/s.");
+                Console.WriteLine("When you are ready to continue press enter");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        } while (userChoice != 5);
     }
 }
