45fd561bb859aef16f29f8b0ba06c81730e67601
hieusStorm <hyrum555@gmail.com>
2024-05-20 11:56:06 -0700

    Made the polymorphism learning project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square testSquare = new Square("Red", 5);
+        Console.WriteLine(testSquare.GetColor());
+        Console.WriteLine(testSquare.GetArea());
+
+        Rectangle testRectange = new Rectangle("rEd", 5, 10);
+        Console.WriteLine(testRectange.GetColor());
+        Console.WriteLine(testRectange.GetArea());
+
+        Circle testCircle = new Circle("red", 5);
+        Console.WriteLine(testCircle.GetColor());
+        Console.WriteLine(testCircle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(testSquare);
+        shapes.Add(testRectange);
+        shapes.Add(testCircle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
