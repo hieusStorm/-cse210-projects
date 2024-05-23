3e21c241674ff0b6a96d6c849804668c2e23bece
hieusStorm <hyrum555@gmail.com>
2024-05-23 11:32:35 -0700

    corrected the string representation of the class

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -14,6 +14,6 @@ public class EternalGoal : Goal
     }
     public override string GetStringRepresentation()
     {
-        return $"EternalGoal:{GetName()},{GetDescription()},{GetPoints}";
+        return $"EternalGoal:{GetName()},{GetDescription()},{GetPoints()}";
     }
 }
